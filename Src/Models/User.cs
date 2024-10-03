32d5e0419aee9aaf5b559f81ec13131cac06bcb8
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2024-10-02 21:53:01 -0300

    feat: add user model

---
+++ Src/Models/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace catedra1.Src.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Rut { get; set; } = string.Empty;
+        [StringLength(100, MinimumLength = 3,
+        ErrorMessage = "Name must be between 3 and 100 characters.")]
+        public string Name { get; set; } = string.Empty;
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
+        [RegularExpression(@"male|female|other|prefer not to say",
+        ErrorMessage = "Gender must be 'male', 'female', 'other', or 'prefer not to say'.")]
+        public string Gender { get; set; } = string.Empty;
+        [PastDate]
+        public DateTime Birthday { get; set; }
+    }
+}
